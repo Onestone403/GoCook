7118706c32c0fa0df2ada39700e2abfacd21d701
Michael <mpf403@googlemail.com>
2023-06-08 12:44:01 +0200

    basic jwt authentication implemented

---
+++ src/gocook/jwt.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/golang-jwt/jwt"
+)
+
+var secret = []byte("test")
+
+func verifyJWT(endpointHandler func(writer http.ResponseWriter, request *http.Request)) http.HandlerFunc {
+
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		if request.Header["Token"] != nil {
+			token, err := jwt.Parse(request.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
+				_, ok := token.Method.(*jwt.SigningMethodHMAC)
+				if !ok {
+					writer.WriteHeader(http.StatusUnauthorized)
+					_, err := writer.Write([]byte("You're Unauthorized!"))
+					if err != nil {
+						return nil, err
+					}
+				}
+				return secret, nil
+
+			})
+			if err != nil {
+				writer.WriteHeader(http.StatusUnauthorized)
+				_, err2 := writer.Write([]byte("You're Unauthorized due to error parsing the JWT"))
+				if err2 != nil {
+					return
+				}
+				return
+			}
+			if token.Valid {
+				endpointHandler(writer, request)
+			} else {
+				writer.WriteHeader(http.StatusUnauthorized)
+				_, err := writer.Write([]byte("You're Unauthorized due to invalid token"))
+				if err != nil {
+					return
+				}
+			}
+		} else {
+			writer.WriteHeader(http.StatusUnauthorized)
+			_, err := writer.Write([]byte("You're Unauthorized due to No token in the header"))
+			if err != nil {
+				return
+			}
+		}
+
+	})
+}
